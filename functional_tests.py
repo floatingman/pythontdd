83d44666f7c1f4746f0a70dbf23f20c1a3d9ad5a
Daniel Newman <dwnewman78@gmail.com>
2015-03-20 12:48:16 -0500

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
-
+                
     def tearDown(self):
         self.browser.quit()
 
