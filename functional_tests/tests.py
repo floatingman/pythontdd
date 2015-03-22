2b36353f6b6e99072e5fcf8171d28d77c5c634fe
Daniel Newman <dwnewman78@gmail.com>
2015-03-22 13:42:32 -0500

    implementing new functionality for creating list simplified home template, added a new view, changed urls.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -40,7 +45,7 @@ class NewVisitorTest(LiveServerTestCase):
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        
+                
         # There is still a text box inviting her to add another item She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
