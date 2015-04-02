69c389505b612e2edd6d0a580ec96e3958542c89
Daniel Newman <dwnewman78@gmail.com>
2015-04-02 15:44:24 -0500

    added logging for faile persona request'

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,3 +1,5 @@
+import logging
+logger = logging.getLogger(__name__)
 import requests
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -19,9 +21,14 @@ class PersonaAuthenticationBackend(object):
                 return User.objects.get(email=email)
             except User.DoesNotExist:
                 return User.objects.create(email=email)
+        else:
+            logger.warning(
+                'Persona says no. Json was: {}'.format(response.json())
+            )
 
     def get_user(self, email):
         try:
             return User.objects.get(email=email)
         except User.DoesNotExist:
             return None
+        
--- superlists/settings.py
+++ superlists/settings.py
@@ -96,3 +96,26 @@ AUTH_USER_MODEL = 'accounts.User'
 AUTHENTICATION_BACKENDS = (
     'accounts.authentication.PersonaAuthenticationBackend',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+        'accounts': {
+            'handlers': ['console'],
+        },
+        'lists': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
